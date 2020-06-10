e20715fe25e7000a478016957b2d266ffa01a875
Lucas Martins <lm.m4rtins@gmail.com>
2020-06-10 19:38:59 -0300

    Conectando com o back e arrumando a tela

--- WEB/src/pages/RegistroUsuario/index.js
+++ WEB/src/pages/RegistroUsuario/index.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
-import { Col, Row,} from 'reactstrap';
+import { Col, Row, } from 'reactstrap';
 
 import './styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,11 +9,16 @@ import Dropdown from 'react-bootstrap/Dropdown'
 import ImageUploader from 'react-images-upload';
 
 import { FiUser, FiLogIn } from 'react-icons/fi'
-import {useHistory, Link} from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 
-import api from '../../services/api';
+import { register } from '../../services/user';
 
-  export default function RegistroUsuario(){
+//toolbar import
+import Backdrop from '../../components/Backdrop/Backdrop'
+import Toolbar from '../../components/Toolbar/Toolbar'
+import SideDrawer from '../../components/SideDrawer/SideDrawer'
+
+export default function RegistroUsuario() {
   const color = {
     color: 'red'
   };
@@ -29,10 +34,10 @@ import api from '../../services/api';
 
   const history = useHistory();
 
-  async function handleRegister(e){
+  async function handleRegister(e) {
     e.preventDefault();
 
-    const data= {
+    const data = {
       name,
       surname,
       birthday,
@@ -43,9 +48,10 @@ import api from '../../services/api';
       password,
     };
 
-    try{
-      const response = await api.post('UserJPA', data);
-      
+    try {
+
+      const response = await register.post('#', data);
+
       alert(`Cadastro feito !`);
 
       history.push('/');
@@ -54,274 +60,249 @@ import api from '../../services/api';
     }
   }
 
-      return ( 
+  //Responsividade da Toolbar
 
-        <div className="line">
-        <div className="navbar">
+  const [sideDrawerOpen, setOpen] = useState(false);
 
-         <Link className="logo" to="/"></Link>
+  const handleSideClose = () => setOpen(false);
+  const handleSideOpen = () => setOpen(true);
 
-           <div className="dropdown dropOne">
-            <button className="dropbtn">Eventos </button>
+  let backdrop;
 
-              <div className="dropdown-content">
-                 <a href="#">Publicar Evento</a>
-                 <a href="#">Em Destaque</a>
-               </div>
-           </div>
-
-           <div className="dropdown dropTwo">
-               <button className="dropbtn">Sobre Nós </button>
-
-               <div className="dropdown-content">
-                 <a href="#">Projeto</a>
-                 <a href="#">Quem Somos</a>
-               </div>
-           </div>
-                   
-               <div className="dropdown dropOne">
-
-                 <Link className="user" to="/login">
-                   <FiUser size={40} color="#1ABC9C" />
-                      Login
-                 </Link>
-               </div>
-
-       </div>
-
-<div>
-
-      <Form onSubmit={handleRegister}>         
-<br></br>
-<br></br>    
-<br></br>
-<br></br>    
-<br></br>
-<br></br>   
-
-<div className="container">
-<Row form>
-           <Col md={2}></Col>
-<Col md={9}>
-        <FormGroup>
-    
-          <h2>Faça seu cadastro</h2>
-          <br></br>
-        </FormGroup>
-     </Col>
-</Row>
-
-
-
-<Row form>
-    <Col md={1}></Col>      
-  <Col md={7}>
-          <h4>1-Dados pessoais</h4>
-          <br></br>
-  </Col>
- </Row>
-
-<Row form>
-<Col md={1}></Col>
-      <Col md={3}>
-        <FormGroup>
-        <h5>Nome: <span style={color}>*</span></h5>
-        <Input
-          type="text"
-          name="name"
-          id="name"
-          placeholder=""
-          value={name}
-          onChange={e => setName(e.target.value)}
-          required
-          />  
-      </FormGroup>
-</Col>
-
- <Col md={3}>
-      <FormGroup>
-     
-        <h5>Sobrenome: <span style={color}>*</span></h5>
-        <Input
-          type="text"
-          name="surname"
-          id="surname"
-          placeholder=""
-          value={surname}
-          onChange={e => setSurname(e.target.value)}
-          required
-          />
-      </FormGroup> </Col>  
-</Row>
-
-<Row form>
-<Col md={1}></Col>
-      <Col md={3}>
-        <FormGroup>
-        <h5>Data de nascimento: <span style={color}>*</span></h5>
-        <Input
-          type="date"
-          name="birthday"
-          id="birthday"
-          value={birthday}
-          onChange={e => setBirthday(e.target.value)}
-          required
-          />  
-      </FormGroup>
-</Col>
-
- <Col md={3}>
-      <FormGroup>
-
-        <h5>Gênero:</h5>
-        <Input type="select" name="select" id="exampleSelect">
-
-           <option 
-           value="#"
-           >Escolha...
-           </option>
-
-           <option 
-           value={sex}
-           onChange={e => setSex(e.target.value)}
-           >M
-           </option>
-
-           <option
-           value={sex}
-           onChange={e => setSex(e.target.value)}
-           >F
-           </option>
-
-        </Input>
-
-      </FormGroup> </Col>  
-</Row>
-
-<Row form>
-<Col md={1}></Col>
-      <Col md={3}>
-        <FormGroup>
-        <h5>CPF: <span style={color}>*</span></h5>
-        <Input
-          type="number"
-          name="cpf"
-          id="cpf"
-          placeholder="000.000.000-00"
-          value={cpf}
-          onChange={e => setCpf(e.target.value)}
-          required
-          />  
-      </FormGroup>
-</Col>
-
-</Row>
-
-
-<Row form>
-<Col md={1}></Col>
-      <Col md={3}>
-        <FormGroup>
-        <h5>Celular: <span style={color}>*</span></h5>
-        <Input
-          type="number"
-          name="phone"
-          id="phone"
-          placeholder="(00)90000-0000"
-          value={phone}
-          onChange={e => setPhone(e.target.value)}
-          required
-          />  
-      </FormGroup>
-</Col>
-</Row>
-
-
-<br></br>
-
-
-<Row form>
-           <Col md={1}></Col>
-           
-
-<Col md={7}>
-                  <h4>2-Dados de conta</h4>
-                  <br></br>
-
-     </Col>
-</Row>
-
-<Row form>
-<Col md={1}></Col>
-      <Col md={3}>
-        <FormGroup>
-        <h5>E-mail: <span style={color}>*</span></h5>
-        <Input
-          type="email"
-          name="email"
-          id="email"
-          placeholder=""
-          required
-          />  
-      </FormGroup>
-</Col>
-</Row>
-
-<Row form>
-
-<Col md={1}></Col>
-      <Col md={3}>
-        <FormGroup>
-        <h5>Senha: <span style={color}>*</span></h5>
-        <Input
-          type="password"
-          name="password"
-          id="password"  
-          placeholder="*********"
-          required        
-          />  
-      </FormGroup>
-</Col>
- <Col md={3}>
-      <FormGroup>
-     
-        <h5>Confime sua senha: <span style={color}>*</span></h5>
-        <Input
-          type="password"
-          name="confirmPassword"
-          id="confirmPassword"
-          placeholder="*********"
-          required
-          />
-      </FormGroup> </Col>  
-</Row>
-
-
-  <div> 
-  <Row form>
-  <Col md={9}></Col>
-    <Col md={2}>  
-  
-  <button className="button" type="submit">Continuar</button>
-</Col> 
-  </Row>
-  </div>
-
-
-  
-  <br></br> 
-  <br></br>
-   </div> 
-
-
-      </Form>
-          </div>
-          </div>
-
-    );
+  if (sideDrawerOpen) {
+    backdrop = <Backdrop click={handleSideClose} />
   }
 
-  
+  return (
+    <>
+      <Toolbar handleSideOpen={handleSideOpen} />
+      <SideDrawer show={sideDrawerOpen} />
+      {backdrop}
 
- 
+
+      <div className="bg-register">
+
+        <Form onSubmit={handleRegister}>
+          
+
+          <div className="form-container">
+            <Row form>
+              <Col md={9}>
+                <FormGroup>
+                  <div className="form-header">Vamos a criar a sua conta pessoal</div>
+                </FormGroup>
+              </Col>
+            </Row>
+
+            <Row form>
+              <Col md={1}></Col>
+              <Col md={7}>
+                <div className="sub-title">1 - Dados pessoais</div>
+              </Col>
+            </Row>
+
+            <Row form>
+              <Col md={1}></Col>
+              <Col md={3}>
+                <FormGroup>
+                  <div className="input-label">Nome: <span style={color}>*</span></div>
+                  <Input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder=""
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    required
+                  />
+                </FormGroup>
+              </Col>
+
+              <Col md={3}>
+                <FormGroup>
+
+                <div className="input-label">Sobrenome: <span style={color}>*</span></div>
+                  <Input
+                    type="text"
+                    name="surname"
+                    id="surname"
+                    placeholder=""
+                    value={surname}
+                    onChange={e => setSurname(e.target.value)}
+                    required
+                  />
+                </FormGroup> </Col>
+            </Row>
+
+            <Row form>
+              <Col md={1}></Col>
+              <Col md={3}>
+                <FormGroup>
+                  <h5>Data de nascimento: <span style={color}>*</span></h5>
+                  <Input
+                    type="date"
+                    name="birthday"
+                    id="birthday"
+                    value={birthday}
+                    onChange={e => setBirthday(e.target.value)}
+                    required
+                  />
+                </FormGroup>
+              </Col>
+
+              <Col md={3}>
+                <FormGroup>
+
+                  <h5>Gênero:</h5>
+                  <Input type="select" name="select" id="exampleSelect">
+
+                    <option
+                      value="#"
+                    >Escolha...
+           </option>
+
+                    <option
+                      value={sex}
+                      onChange={e => setSex(e.target.value)}
+                    >M
+           </option>
+
+                    <option
+                      value={sex}
+                      onChange={e => setSex(e.target.value)}
+                    >F
+           </option>
+
+                  </Input>
+
+                </FormGroup> </Col>
+            </Row>
+
+            <Row form>
+              <Col md={1}></Col>
+              <Col md={3}>
+                <FormGroup>
+                  <h5>CPF: <span style={color}>*</span></h5>
+                  <Input
+                    type="text"
+                    name="cpf"
+                    id="cpf"
+                    maxLength="11"
+                    size="11"
+                    placeholder="000.000.000-00"
+                    value={cpf}
+                    onChange={e => setCpf(e.target.value)}
+                    required
+                  />
+                </FormGroup>
+              </Col>
+
+            </Row>
+
+
+            <Row form>
+              <Col md={1}></Col>
+              <Col md={3}>
+                <FormGroup>
+                  <h5>Celular: <span style={color}>*</span></h5>
+                  <Input
+                    type="number"
+                    name="phone"
+                    id="phone"
+                    placeholder="(00)90000-0000"
+                    value={phone}
+                    onChange={e => setPhone(e.target.value)}
+                    required
+                  />
+                </FormGroup>
+              </Col>
+            </Row>
+
+
+            <br></br>
+
+
+            <Row form>
+              <Col md={1}></Col>
+
+
+              <Col md={7}>
+                <h4>2-Dados de conta</h4>
+                <br></br>
+
+              </Col>
+            </Row>
+
+            <Row form>
+              <Col md={1}></Col>
+              <Col md={3}>
+                <FormGroup>
+                  <h5>E-mail: <span style={color}>*</span></h5>
+                  <Input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder=""
+                    required
+                  />
+                </FormGroup>
+              </Col>
+            </Row>
+
+            <Row form>
+
+              <Col md={1}></Col>
+              <Col md={3}>
+                <FormGroup>
+                  <h5>Senha: <span style={color}>*</span></h5>
+                  <Input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="*********"
+                    required
+                  />
+                </FormGroup>
+              </Col>
+              <Col md={3}>
+                <FormGroup>
+
+                  <h5>Confime sua senha: <span style={color}>*</span></h5>
+                  <Input
+                    type="password"
+                    name="confirmPassword"
+                    id="confirmPassword"
+                    placeholder="*********"
+                    required
+                  />
+                </FormGroup> </Col>
+            </Row>
+
+
+            <div>
+              <Row form>
+                <Col md={9}></Col>
+                <Col md={2}>
+
+                  <button className="button" type="submit">Continuar</button>
+                </Col>
+              </Row>
+            </div>
+
+
+
+            <br></br>
+            <br></br>
+          </div>
+
+
+        </Form>
+      </div>
+    </>
+  );
+}
+
+
+
+
 
--- WEB/src/services/user.js
+++ WEB/src/services/user.js
@@ -2,5 +2,9 @@ import axios from 'axios'
 
 const login = ({id}) => axios.post(`https://eventech.outsystemscloud.com/Core/rest/User/v1/${id}`)
 
+const register = axios.create({
+    baseURL:'https://eventech.outsystemscloud.com/Core/rest/User/v1'
+});
 
-export default login;
+
+export {login, register};
